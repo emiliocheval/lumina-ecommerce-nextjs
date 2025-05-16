de5145d393e7a18bc32784fd391701c86ed8c733
Emil <emil.conradsson@hotmail.com>
2025-05-16 22:15:20 +0200

    fixed depricated export const config

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -82,10 +82,4 @@ export async function POST(req: NextRequest) {
   }
 
   return NextResponse.json({ received: true });
-}
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}; 
+} 
